afd89e718037308bd17c65103621492b80446cf1
Benjamin Stookey <ben.stookey@gmail.com>
2020-04-16 07:55:36 -0700

    Updates

---
+++ precache-manifest.dacb3b8e31e9a7ece96ff5328e592a95.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c4dba25f6780c21cb4cc87b65b1be84",
+    "url": "/covid-timeline/index.html"
+  },
+  {
+    "revision": "216507084084e659dedd",
+    "url": "/covid-timeline/static/css/main.5e0ac16f.chunk.css"
+  },
+  {
+    "revision": "40a32b6e59c609dd92f8",
+    "url": "/covid-timeline/static/js/2.66323197.chunk.js"
+  },
+  {
+    "revision": "216507084084e659dedd",
+    "url": "/covid-timeline/static/js/main.fa84da44.chunk.js"
+  },
+  {
+    "revision": "c42be48175ced9627d44",
+    "url": "/covid-timeline/static/js/runtime~main.51465983.js"
+  }
+]);
