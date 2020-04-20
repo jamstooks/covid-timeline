4dca8f9422420ee835662347e3a4d2487dc02b9f
Benjamin Stookey <ben.stookey@gmail.com>
2020-04-20 09:23:01 -0700

    Updates

---
+++ precache-manifest.5c954eb0ac13e5f2191916f9b0775582.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79983a4410c9e1783ec8f527ab5f1bcf",
+    "url": "/covid-timeline/index.html"
+  },
+  {
+    "revision": "c50a3bd9c7b4ca703ed4",
+    "url": "/covid-timeline/static/css/main.5e0ac16f.chunk.css"
+  },
+  {
+    "revision": "17962154256dbf112125",
+    "url": "/covid-timeline/static/js/2.29fd21fe.chunk.js"
+  },
+  {
+    "revision": "c50a3bd9c7b4ca703ed4",
+    "url": "/covid-timeline/static/js/main.3687424a.chunk.js"
+  },
+  {
+    "revision": "c42be48175ced9627d44",
+    "url": "/covid-timeline/static/js/runtime~main.51465983.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid-timeline/precache-manifest.48a227b9c15c73228783682717aef257.js"
+  "/covid-timeline/precache-manifest.5c954eb0ac13e5f2191916f9b0775582.js"
 );
 
 self.addEventListener('message', (event) => {
